1d973030c313145389fe875cf0e3ce5a30db1923
Suyash Adhikari <suyashadhikari99@gmail.com>
2026-05-22 05:14:13 -0400

    fix: remove trailing blank line and fix long lines

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -81,8 +81,6 @@ class TestAccountService(TestCase):
         """It should get 200_OK from the Home Page"""
         response = self.client.get("/")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(
-                response.headers.get('Access-Control-Allow-Origin'), '*')
 
     def test_health(self):
         """It should be healthy"""
@@ -135,23 +133,21 @@ class TestAccountService(TestCase):
         """It should return security headers"""
         response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(
-                response.headers.get('Access-Control-Allow-Origin'), '*')
         headers = {
             'X-Frame-Options': 'SAMEORIGIN',
             'X-Content-Type-Options': 'nosniff',
             'Content-Security-Policy': (
-                'default-src \'self\'; object-src \'none\'',
+                'default-src \'self\'; object-src \'none\''
                 ),
             'Referrer-Policy': 'strict-origin-when-cross-origin'
         }
         for key, value in headers.items():
             self.assertEqual(response.headers.get(key), value)
 
-        # Check for the CORS header
     def test_cors_security(self):
         """It should return a CORS header"""
         response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+        # Check for the CORS header
         self.assertEqual(
                 response.headers.get('Access-Control-Allow-Origin'), '*')
